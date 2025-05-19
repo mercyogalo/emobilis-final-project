11c074622d32e720b0d44978dee6f66074c74df2
mercyogalo <ogalomercy8@gmail.com>
2025-05-19 15:46:02 +0300

    UPdated the website to use an env file and also postgres

--- farm/farm/settings.py
+++ farm/farm/settings.py
@@ -72,30 +72,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "farm.wsgi.application"
 
+from decouple import config
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": 'farm',
-#         "USER":'root',
-#         "PASSWORD":'',
-#         "HOST":'localhost',
-#         "PORT":'3306',
-#           'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'"
-#         },
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":config('DB_NAME'),
+       "USER":config('DB_USER'),
+       "PASSWORD":config('DB_PASSWORD'),      
+        "HOST":config('DB_HOST'),
+        "PORT":config('DB_PORT'),         
+       },
+   } 
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
